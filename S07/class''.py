d1f393938f3ab2d083db56ae296f6172724dd960
elenaa <e.llames.2023@alumnos.urjc.es>
2024-02-20 16:45:30 +0100

    This is going to be my first but not last commit

---
+++ S07/class''.py
@@ -0,0 +1,15 @@
+class Car:
+    def __init__(self, brand, speed=0):
+        self.car_brand = brand
+        self.speed = speed
+
+    def set_speed(self, brand):
+        self.speed = speed
+class Ferrari(Car):
+    pass
+
+mycar = Car("Renault")
+yourcar = Ferrari("Ferrari")
+print(yourcar.car_brand)
+yourcar.set_speed(120)
+print(yourcar.speed)

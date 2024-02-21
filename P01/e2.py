5050cc427eb5f1288c27597ce7c74e12a889aba6
elenaa <e.llames.2023@alumnos.urjc.es>
2024-02-21 18:38:34 +0100

    This is going to be my first but not last commit

--- P01/e2.py
+++ P01/e2.py
@@ -1,18 +1,49 @@
+DNA_BASES = ["A", "T", "C", "G"]
+
+
+def print_seqs(seq_list):
+    n = 0
+    for seq in seq_list:
+        print(f"Sequence {n} : (Length: {len(seq)}) {seq} \n")
+        n += 1
+
+
+def generate_seqs(pattern, number):
+    seq_list = []
+    seq = ""
+    for i in range(number):
+        seq += pattern
+        seq_list.append(Seq(seq))
+    return seq_list
+
+
 class Seq:
-    def __init__(self, strbases):
-        count = 0
-        for e in strbases:
-            if e == "A":
-                count += 1
-            if e == "C":
-                count += 1
-            if e == "G":
-                count += 1
-            if e == "T":
-                count += 1
-        if count == len(strbases):
-            self.strbases = strbases
-            print("New sequence created!")
+    def __init__(self, strbases=None):
+        if strbases is None:
+            print("NULL sequence created")
+            self.strbases = "NULL"
+        else:
+            count = 0
+            for base in DNA_BASES:
+                count += strbases.count(base)
+
+            if count == len(strbases):
+                self.strbases = strbases
+                print("New sequence created!")
+            else:
+                print("INVALID sequence created")
+                self.strbases = "ERROR"
+
+
+    def __len__(self):
+        if self.strbases == "NULL" or self.strbases == "ERROR":
+            return 0
+        else:
+            return len(self.strbases)
+
+
+    def __str__(self):
+        return self.strbases
 
 
 # -- Creating a Null sequence

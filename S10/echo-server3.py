54054f8e7a610582232cf687b0998b75ae87ba8f
elenaa <e.llames.2023@alumnos.urjc.es>
2024-03-05 16:50:21 +0100

    This is going to be my first but not last commit

--- S10/echo-server3.py
+++ S10/echo-server3.py
@@ -1,13 +1,13 @@
 import socket
-
+import termcolor
 # Configure the Server's IP and PORT
 PORT = 8081
-IP = ""  # it depends on the machine the server is running
+IP = "212.128.255.88" # this IP address is local, so only requests from the same machine are possible
+
 MAX_OPEN_REQUESTS = 5
 
 # Counting the number of connections
 number_con = 0
-
 # create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
@@ -28,21 +28,33 @@ try:
 
         # Print the connection number
         print(f"CONNECTION: {number_con}. From the IP: {address}")
-
-        # Read the message from the client, if any
-        msg = clientsocket.recv(2048).decode("utf-8")  # recibo el mensaje en bytes
-        print(f"Message from client: {msg}")
-
-        # Send the message
-        message = "Hello from the teacher's server\n"
-        send_bytes = str.encode(message)
-        # We must write bytes, not a string
-        clientsocket.send(send_bytes)
-        clientsocket.close()
-
 except socket.error:
     print(f"Problems using ip {IP} port {PORT}. Is the IP correct? Do you have port permission?")
 
 except KeyboardInterrupt:
     print("Server stopped by the user")
     serversocket.close()
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening, server ready to listen different request
+ls.listen()
+
+print("The server is configured!")
+
+while True:
+    (rs, address) = ls.accept()
+    print("Waiting for Clients to connect echo")
+    print(f"A client has connected to the server!")
+    print("Client IP,PORT: ", )
+    msg = rs.recv(2048).decode("utf-8")
+    print("Message received: " + termcolor.colored(msg, "green"))
+    newMsg = "ECHO:  " + msg
+    rs.send(newMsg.encode())
+    rs.close()
+# -- Close the socket
+ls.close()
